bea795c7c3dc910e312bf775dce60d118aa6fb0d
Lexi Pitney <pitneyam@miamioh.edu>
2024-03-03 20:03:30 -0500

    Information added to footer

--- wildflowergoats/src/components/Footer.js
+++ wildflowergoats/src/components/Footer.js
@@ -3,7 +3,13 @@ function Footer(){
     return (
         <div>
             <div className="footer">
-                <p>Footer</p>
+                <h5>Contact Us!</h5>
+                <a>Phone: </a>
+                <a href="tel:419-234-0756">(419)234-0756</a>
+                <a>Email: </a>
+                <a href="mailto:"></a>
+                <a>Follow us on Instagram: </a>
+                <a href='https://www.instagram.com/wildflower_family_farm/'> @wildflower_family_farm</a>
             </div>
         </div>
     );
