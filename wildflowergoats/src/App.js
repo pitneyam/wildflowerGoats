0b4c00bfeee63de934e2bcb51db7a8fdc17ce1ba
Lexi Pitney <pitneyam@miamioh.edu>
2024-03-03 19:57:36 -0500

    got everything to respective places within components

--- wildflowergoats/src/App.js
+++ wildflowergoats/src/App.js
@@ -1,14 +1,20 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import entrance from './images/farm_entrance.jpg';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <Nav></Nav>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*"/>
+
+        </Routes>
+      </BrowserRouter>
       
       <Footer></Footer>
     </div>
--- wildflowergoats/src/components/Home.js
+++ wildflowergoats/src/components/Home.js
@@ -1,8 +1,11 @@
+import entrance from '../images/farm_entrance.jpg';
 
-function Home{
+function Home(){
     return(
         <header className="App-header">
             <img src={entrance} className="header_pic" alt="farm_entrance" />
         </header>
-    )
+    );
 }
+
+export default Home;
